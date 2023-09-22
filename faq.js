f0c88f619d16cdd7de11534fbd56913435927c65
xeidan <138797410+xeidan@users.noreply.github.com>
2023-09-22 20:16:17 +0100

    Add files via upload

---
+++ faq.js
@@ -0,0 +1,9 @@
+// JavaScript to toggle the answers
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(question => {
+  question.addEventListener('click', () => {
+    const answer = question.nextElementSibling;
+    answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
+  });
+});
